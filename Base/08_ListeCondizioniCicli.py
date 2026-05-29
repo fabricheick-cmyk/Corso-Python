95aeb75c75912ef6b1f7ec2c436cab4e607ffae9
fabricheick-cmyk <fabri.cheick@gmail.com>
2026-05-29 10:21:26 +0200

    Update

--- Base/08_ListeCondizioniCicli.py
+++ Base/08_ListeCondizioniCicli.py
@@ -17,42 +17,14 @@ print(Studenti)
 Studenti.append("Margaret")
 print(Studenti)
 
+# Accesso agli elementi
+print(Studenti[2])
+print(Studenti[5])
 
-"""
-# Condizione If(se) elif(altrimenti se) else(altrimenti)
 
-if condizione: 
-    Codice
-else:
-    Codice
 
-if condizione:
-     Codice
-elif condizione:
-     Codice:
-else:
-    Codice                 
-"""
 
-cliente = input("Hai una prenotazione")
-if cliente == "si":
-    print("ok")
-else:
-    print("Non ci sono posti")
 
-# Risultato esame
-Studenti = input("inserisci il nome dello studente")
-votoesame = int(input("inserisci il voto dello studente (da 1 a 33): "))
 
-if votoesame >= 18:
-    print("Esame Superato")
-elif votoesame >= 28:
-    print("Ottimo Lavoro")    
-elif votoesame > 30:
-    print("Lode")
-else:
-    print("Bocciato")
-
-print       
  
 
